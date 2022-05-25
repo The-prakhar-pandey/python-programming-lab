7788c39d1b63adc8f1656edf94a6eb1e046dd2b5
The-prakhar-pandey <91780746+The-prakhar-pandey@users.noreply.github.com>
2022-05-25 23:48:01 +0530

    greatest among three numbers

---
+++ greatest among 3.py
@@ -0,0 +1,13 @@
+x=int(input("enter value"))
+y=int(input("enter value"))
+z=int(input("enter value"))
+if x>y>z:
+    print("x is greatest")
+elif y>x>z:
+    print("y is greatest")
+else:
+    print("z is greatest")
+
+
+
+
