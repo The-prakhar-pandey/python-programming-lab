e157f4395602d55102944cf28024df70d09a127f
The-prakhar-pandey <91780746+The-prakhar-pandey@users.noreply.github.com>
2022-05-25 21:52:36 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+for i in range(1001):
+    num=i
+    result=0
+    n=len(str(i))
+    while(i!=0):
+        digit=i%10
+        result+=digit**n
+        i=i//10
+    if num==result:
+        print(num)
+
+        
