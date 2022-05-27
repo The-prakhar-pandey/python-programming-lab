60dc6c643175343af90a9aee8cc69925cafc6150
The-prakhar-pandey <91780746+The-prakhar-pandey@users.noreply.github.com>
2022-05-27 23:27:34 +0530

    swaping

---
+++ swap.py
@@ -0,0 +1,8 @@
+#to swap two number 3rd variable
+x=int(input("enter value"))
+y=int(input("enter value"))
+temp=x
+x=y
+y=temp
+print("after swapping the value of x",x)
+print("after swapping the value of y",y)
