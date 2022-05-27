13ab5869552851b67efa4d2cdc84b61ae0deee3b
The-prakhar-pandey <91780746+The-prakhar-pandey@users.noreply.github.com>
2022-05-27 23:25:05 +0530

    list sum and avg

---
+++ lst sum nd avg.py
@@ -0,0 +1,12 @@
+#we have to calculate average and sum of element in a list
+
+s=0
+x=list(map(int,input('enter value').split()))
+for i in x:
+    s=s+i
+    a=s/2
+print(s)
+print(a)
+
+    
+    
